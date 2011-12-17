b80d73c69ae19b1e10e2daefb34342e95193bdbf
Hank Stoever <hstove@gmail.com>
2011-12-16 21:31:39 -0500

    functionality

--- jq.js
+++ jq.js
@@ -1,7 +1,36 @@
 $.template("track", "<div id='trackbox'><a data-url='${stream_url}' href='#'>${title}</a>");
 
 $(document).ready(function(){
+  $("#container img").hide();
   $('#q').keyup(function(){
+    $.ajax({
+      url: "https://api.soundcloud.com/tracks.json?" + $('#search').serialize(),
+      dataType: 'json',
+      beforeSend: 
+        function(data){
+          $("#container img").show();
+        },
+      success:
+        function(data){
+          var items = [];
+          $.each(data, function(key, val){
+            items.push("<div id='trackbox'><h2><a data-artist='"+val.user.username+"' data-url=" + val.stream_url + " href='#'>"+val.title+"</a></h2>\
+            <span class='plays'>" + val.playback_count+ " plays</span></br>\
+            <h4>" + val.user.username+ "</h4></div>");
+          });
+          $("#container img").hide();
+          $('#results').html(items.join(' '));
+          getReady();
+        }
+    });
+    $('.btn').click(function(){
+      var current = $(this).html;
+      if (current === 'Play'){
+        $(this).html('pause');
+      };
+    });
+  });
+  $("#search").submit(function(){
     $.ajax({
       url: "https://api.soundcloud.com/tracks.json?" + $('#search').serialize(),
       dataType: 'json',
@@ -9,7 +38,7 @@ $(document).ready(function(){
         function(data){
           var items = [];
           $.each(data, function(key, val){
-            items.push("<div id='trackbox'><h2><a data-url=" + val.stream_url + " href='#'>"+val.title+"</a></h2>\
+            items.push("<div id='trackbox'><h2><a data-plays='"+val.playback_count+"' data-url=" + val.stream_url + " href='#'>"+val.title+"</a></h2>\
             <span class='plays'>" + val.playback_count+ " plays</span></br>\
             <h4>" + val.user.username+ "</h4></div>");
           });
@@ -17,14 +46,31 @@ $(document).ready(function(){
           getReady();
         }
     });
-  });
+    return false;
+  })
 });
 var clientid = "client_id=3191b63184cbb88d54a36d1e6afc3a00";
 
 function getReady(){
   $('#trackbox a').click(function(){
     var url= $(this).attr('data-url') +"?"+ clientid;
+    var title= $(this).html();
+    var artist = $(this).attr('data-artist');
+    $('#player h2').html(title);
+    $('#player h4').html(artist);
     changeSrc(url);
     return false;
   });
 }
+
+function changeSrc(url){
+  var audioPlayer = document.getElementsByTagName('audio')[0];
+  audioPlayer.src = url;
+  audioPlayer.load();
+  audioPlayer.play();
+  $('.btn').html('pause')
+}
+
+function playPause
+
+
--- js.js
+++ js.js
@@ -1,6 +1,2 @@
-function changeSrc(url){
-  var audioPlayer = document.getElementsByTagName('audio')[0];
-  audioPlayer.src = url;
-  audioPlayer.load();
-  audioPlayer.play();
-}
+
+
