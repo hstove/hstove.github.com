ca623e0131560d3eb1459398b9b42465d6998c35
Hank Stoever <hstove@gmail.com>
2011-12-16 20:11:10 -0500

    styling

--- jq.js
+++ jq.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
           var items = [];
           $.each(data, function(key, val){
             items.push("<div id='trackbox'><h2><a data-url=" + val.stream_url + " href='#'>"+val.title+"</a></h2>\
-            <span class='plays'>" + val.playback_count+ "</span></br>\
+            <span class='plays'>" + val.playback_count+ " plays</span></br>\
             <h4>" + val.user.username+ "</h4></div>");
           });
           $('#results').html(items.join(' '));
@@ -25,5 +25,6 @@ function getReady(){
   $('#trackbox a').click(function(){
     var url= $(this).attr('data-url') +"?"+ clientid;
     changeSrc(url);
-  })
+    return false;
+  });
 }
